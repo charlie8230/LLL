6cd5ccc5d0bc60a57d7b952348c754fbcbdebab1
charlie8230 <carlos@moranjr.com>
2013-10-14 16:40:22 -0400

    Update lll.js

--- lll.js
+++ lll.js
@@ -2,6 +2,18 @@
 	A lazy load script with no jQuery dependencies
 	Script by Carlos Moran
 	License MIT
+
+	The idea is the same as other image lazy loads:
+	make the request to server after the page load completes.
+	On image heavy sites, this will help you get the DOM
+	all nice and loaded without waiting for all the images.
+
+	It requires a data-lsrc attribute on images with the
+	uri for the image.  The src will be initally set to a blank.gif
+	until the users scrolls to the image when it will be fetched.
+
+	The offset is set to load the image x pixels before the user
+	scrolls to the top of the image. 
 */
 
 var ll = function(document, window, soffset, imgs){
